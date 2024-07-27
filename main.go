ba2252f60833912415f9fd58fb7c2486475cad6f
Nathaniel Alcedo <nate@natealcedo.dev>
2024-07-27 19:18:10 +0800

    Add practical example

--- main.go
+++ main.go
@@ -1,54 +1,84 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
+	"sync"
 	"time"
 )
 
+type UserProfile struct {
+	ID       int      `json:"id"`
+	Comments []string `json:"comments"`
+	Likes    int      `json:"likes"`
+	Friends  []int    `json:"friends"`
+}
+
+type Response struct {
+	data any
+	err  error
+}
+
+func handleGetUserProfile(id int) (*UserProfile, error) {
+	responseCh := make(chan *Response, 3)
+	wg := &sync.WaitGroup{}
+	go getComments(id, responseCh, wg)
+	go getLikes(id, responseCh, wg)
+	go getFriends(id, responseCh, wg)
+
+	wg.Add(3)
+	wg.Wait()
+	close(responseCh)
+
+	userProfile := &UserProfile{ID: id}
+	// Keep ranging. But when to stop?
+	for resp := range responseCh {
+		if resp.err != nil {
+			return nil, resp.err
+		}
+		switch msg := resp.data.(type) {
+		case []string:
+			userProfile.Comments = msg
+		case int:
+			userProfile.Likes = msg
+		case []int:
+			userProfile.Friends = msg
+		}
+	}
+
+	return userProfile, nil
+}
+
+func getComments(id int, responseCh chan *Response, wg *sync.WaitGroup) {
+	time.Sleep(200 * time.Millisecond)
+	comments := []string{
+		"yeah buddy",
+		"I didn't know that",
+	}
+	responseCh <- &Response{data: comments, err: nil}
+	wg.Done()
+}
+
+func getLikes(id int, responseCh chan *Response, wg *sync.WaitGroup) {
+	time.Sleep(200 * time.Millisecond)
+	responseCh <- &Response{data: 33, err: nil}
+	wg.Done()
+}
+
+func getFriends(id int, responseCh chan *Response, wg *sync.WaitGroup) {
+	time.Sleep(100 * time.Millisecond)
+	responseCh <- &Response{data: []int{11, 12, 123, 6456}, err: nil}
+	wg.Done()
+}
+
 func main() {
 	start := time.Now()
-	ctx := context.WithValue(context.Background(), "username", "natealcedo")
-
-	val, err := fetchUserId(ctx)
+	userProfile, err := handleGetUserProfile(10)
 
 	if err != nil {
-		_ = fmt.Errorf("error fetching value: %v", err)
 		log.Fatal(err)
 	}
 
-	fmt.Printf("The response is -> %s and took %+v\n", val, time.Since(start))
-}
-
-func fetchUserId(ctx context.Context) (string, error) {
-	ctx, cancel := context.WithTimeout(ctx, time.Millisecond*100)
-	defer cancel()
-
-	username := ctx.Value("username")
-	fmt.Println(username)
-
-	type result struct {
-		userId string
-		err    error
-	}
-
-	resCh := make(chan result, 5)
-
-	go func() {
-		userId, err := thirdPartyFetchValue()
-		resCh <- result{userId, err}
-	}()
-
-	select {
-	case <-ctx.Done():
-		return "", ctx.Err()
-	case res := <-resCh:
-		return res.userId, res.err
-	}
-}
-
-func thirdPartyFetchValue() (string, error) {
-	time.Sleep(time.Millisecond * 90)
-	return "user id 1", nil
+	fmt.Printf("User Profile: %+v\n", userProfile)
+	fmt.Println("Time taken: ", time.Since(start))
 }
